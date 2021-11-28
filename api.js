78652fb748db354bd4efb96e4a020807ca6b6fc4
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-11-28 11:06:24 -0300

    Login test

--- api.js
+++ api.js
@@ -1,8 +1,23 @@
 const http = require("http");
-
+const DEFAULT_USER = { username: "leeodesign", password: "123" };
 const routes = {
   "/contact:get": (request, response) => {
     response.write("Contact us page!");
+    response.end();
+  },
+  "/login:post": async (request, response) => {
+    for await (const data of request) {
+      const user = JSON.parse(data);
+      if (
+        user.username !== DEFAULT_USER.username ||
+        user.password !== DEFAULT_USER.password
+      ) {
+        response.writeHead(401);
+        response.write("Login failed!");
+        response.end();
+      }
+    }
+    response.write("Logging has succeeded!");
     response.end();
   },
   default: (request, response) => {
--- api.test.js
+++ api.test.js
@@ -1,11 +1,27 @@
 const { describe, it } = require("mocha");
 const request = require("supertest");
 const app = require("./api");
+const assert = require("assert");
 describe("API Suite de Test", () => {
   describe("/contact", () => {
     it("Should request the contact page and return HTTP Status 200", async () => {
       const response = await request(app).get("/contact").expect(200);
-      console.log(response);
+      assert.deepStrictEqual(response.text, "Contact us page!");
+    });
+  });
+  describe("/hello", () => {
+    it("Should request an inexistent route /hi and return HTTP Status 404 and redirect", async () => {
+      const response = await request(app).get("/hi").expect(404);
+      assert.deepStrictEqual(response.text, "404 Not Found!");
+    });
+  });
+  describe("/login", () => {
+    it("Should login successfully on the login and return HTTP Status 200", async () => {
+      const response = await request(app)
+        .post("/login")
+        .send({ username: "leeodesign", password: "123" })
+        .expect(200);
+      assert.deepStrictEqual(response.text, "Logging has succeeded!");
     });
   });
 });
